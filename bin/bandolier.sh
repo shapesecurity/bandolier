e825984ded4d8beaead480784b2a5f167cbf1fe1
Kevin Gibbons <bakkot@gmail.com>
2019-04-02 14:25:55 -0700

    forward all arguments to jar from shell script (#61)

--- bin/bandolier.sh
+++ bin/bandolier.sh
@@ -5,4 +5,4 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-java -jar "$DIR/bandolier.jar" "$1"
+java -jar "$DIR/bandolier.jar" "$@"
