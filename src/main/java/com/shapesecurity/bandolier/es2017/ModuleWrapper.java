61decbb535cc043028c2c470140a234a217fddca
Madhukar <mkedlaya@shapesecurity.com>
2020-06-08 16:24:19 -0700

    removing dead code

--- src/main/java/com/shapesecurity/bandolier/es2017/ModuleWrapper.java
+++ src/main/java/com/shapesecurity/bandolier/es2017/ModuleWrapper.java
@@ -5,16 +5,10 @@ import com.shapesecurity.shift.es2017.ast.Module;
 import javax.annotation.Nonnull;
 
 public class ModuleWrapper {
-
 	@Nonnull
 	public Module module;
 
 	public ModuleWrapper(@Nonnull Module module) {
 		this.module = module;
-	}
-
-	@Nonnull
-	public boolean contentEquals(Object object) {
-		return object instanceof ModuleWrapper && this.module.equals(((ModuleWrapper) object).module);
 	}
 }
